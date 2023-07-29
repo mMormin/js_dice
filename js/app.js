279a7de1569de5f910bd5ea3feb6a037b98feebb
mMormin <mmboudot@gmail.com>
2023-07-29 05:25:01 +0200

    D O N E

--- js/app.js
+++ js/app.js
@@ -1,59 +1,67 @@
 /* Création de la div du dealer */
-const playerTwo = document.createElement("div");
+const dealerDiv = document.createElement("div");
 
-/* Association de classe et de l'ID du dealer */
-playerTwo.classList.add("board");
-playerTwo.id = "dealer";
+/* Définition de la classe et de l'ID du dealer */
+dealerDiv.classList.add("board");
+dealerDiv.id = "dealer";
 
-/* Append de la div dans le DOM aprés player */
-document.getElementById("player").after(playerTwo);
+/* Append de la div dans le DOM aprés la div player */
+document.getElementById("player").after(dealerDiv);
 
-////////////////////////////////////////////////////////////////////////////////////////////
-
-/* Demande à l'utilisateur de combien de Dés souhaite-t-il */
+/* Demande à l'utilisateur de combien de Dés souhaite-t-il lancer */
 let parsedResponse = parseInt(prompt("Combien de Dé(s) souhaites tu lancer ?"));
 
-/* Génération d'un Dé en fonction de la réponse de l'utilisateur */
+/* Génération du nombre de Dé(s) choisi par l'utilisateur */
 for (let i = 1; i <= parsedResponse; i++) {
-  newDice();
+  newDice(player, dealer);
 }
 
-/* Function un chiffre aléatoire entre 1 et 6 */
+/* Function générant un chiffre aléatoire entre 1 et 6 */
 function randomInt(min = 1, max = 6) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
-/* Function générant 1 Dé */
-function newDice() {
+/* Function générant 1 Dé pour le player et/ou le dealer */
+function newDice(player, dealer) {
   /* Création de la div avec comprennant les faces du Dé */
-  const dice = document.createElement("div");
+  const playerOneDice = document.createElement("div");
+  const playerTwoDice = document.createElement("div");
 
   /* Association de classe */
-  dice.classList.add("dice");
+  playerOneDice.classList.add("dice");
+  playerTwoDice.classList.add("dice");
 
   /* Append de la div dans le DOM */
-  document.getElementById("player").appendChild(dice);
-  document.getElementById("dealer").appendChild(dice);
+  player = document.getElementById("player").appendChild(playerOneDice);
+  dealer = document.getElementById("dealer").appendChild(playerTwoDice);
 
-  /* Création du style pour chaque face du Dés */
+  /* Création d'un tableau comprenant les Dés des joueurs */
+  const playersDices = document.getElementsByClassName("dice");
+  const playersDicesArray = Array.from(playersDices);
+
+  /* Création du style pour chaque face du Dé */
   const style = document.createElement("style");
   style.innerHTML =
     ".dice__face-1 {background-position-x: 0;} .dice__face-2 {background-position-x: -100px;} .dice__face-3 {background-position-x: -200px;} .dice__face-4 {background-position-x: -300px;} .dice__face-5 {background-position-x: -400px;} .dice__face-6 {background-position-x: -600px;}";
   document.head.appendChild(style);
 
-  let faceNumber = randomInt();
-  /* Association de chaque Dés à son style en fonction du résultat optenu */
-  if (faceNumber === 1) {
-    const faceOne = dice.classList.add("dice__face-1");
-  } else if (faceNumber === 2) {
-    const faceTwo = dice.classList.add("dice__face-2");
-  } else if (faceNumber === 3) {
-    const faceThree = dice.classList.add("dice__face-3");
-  } else if (faceNumber === 4) {
-    const faceFour = dice.classList.add("dice__face-4");
-  } else if (faceNumber === 5) {
-    const faceFive = dice.classList.add("dice__face-5");
-  } else if (faceNumber === 6) {
-    const faceSix = dice.classList.add("dice__face-6");
-  }
+  /* Boucle pour affecter un Dé à chaque joueur */
+  playersDicesArray.forEach(function (playerDice) {
+    /* Génération du nombre du Dé */
+    let faceNumber = randomInt();
+    /* Association de chaque Dés à son style en fonction du résultat optenu */
+    if (faceNumber === 1) {
+      playerDice.classList.add("dice__face-1");
+    } else if (faceNumber === 2) {
+      playerDice.classList.add("dice__face-2");
+    } else if (faceNumber === 3) {
+      playerDice.classList.add("dice__face-3");
+    } else if (faceNumber === 4) {
+      playerDice.classList.add("dice__face-4");
+    } else if (faceNumber === 5) {
+      playerDice.classList.add("dice__face-5");
+    } else if (faceNumber === 6) {
+      playerDice.classList.add("dice__face-6");
+    }
+  });
 }
